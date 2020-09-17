a210efa113bebe460275bb6d6f52989b2c212664
gustavomelo20 <infogustavomelo@gmail.com>
2020-09-17 17:54:30 -0300

    atulizando index json

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-var nome = document.querySelector("#exampleInputName");
 
-nome.value="Gustavo";
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gander]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail"); 
+var passworld = document.querySelector("#exampleInputPassworld");
+var file = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+var fields = document.querySelectorAll("#form-user-create [name]");
+var  user = {};
+
+function addLine(dataUser);
+
+document.getElementById("form-user-create").addEventListener("submit", function(envet){
+
+    envet.preventDefault();
+
+
+    fields.forEach(function(field, index){
+
+	if (field.name == "gender"){
+		if (field.checked === true)
+		 user[field.name] = field.value;
+	} else {
+		 user[field.name] = field.value;
+	}
+
+    ;
+
+    });
+
+    addLine(user); 
+	 
+});
+
